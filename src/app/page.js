68cf38fc8367627857a2222c7a05640d9a670428
sarthakskumar <sskworld9742@gmail.com>
2023-10-02 13:55:27 +0530

    feat: configure Tailwind CSS settings

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,6 @@
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className="w-screen h-screen">
       <h1>QuickSend</h1>
     </main>
   );
