d4195bb0f969dc1c900fd4343536d4b32337fdbd
sarthakskumar <sskworld9742@gmail.com>
2023-10-18 23:02:26 +0530

    chore: remove head tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,12 +12,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        {" "}
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.png"></link>
-        <meta name="theme-color" content="#fff" />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
