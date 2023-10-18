a83f1587105d0c01b1f6fa3b5678e4a2bb368ee5
sarthakskumar <sskworld9742@gmail.com>
2023-10-18 22:59:20 +0530

    feat: create PWA

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,11 +6,18 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Quick Send",
   description: "Send Whatsapp/Telegram Messages to your contacts quickly",
+  manifest: "/manifest.webmanifest",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {" "}
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <meta name="theme-color" content="#fff" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
